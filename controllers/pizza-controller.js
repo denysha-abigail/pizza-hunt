3568891fa179f0c26def104e51d920135e17b12c
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-06-18 02:33:00 -0400

    created the pizza controller

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,8 @@
+const { Pizza } = require('../models');
+
+const pizzaController = {
+    // functions will go here as methods
+};
+
+// tip: when setting up a file, create the data you'll be exporting and then immediately write the export command
+module.exports = pizzaController;
