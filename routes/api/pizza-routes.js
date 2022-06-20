c40edd0f5984b323427648fd7a1dd2e46230b496
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-06-20 02:13:53 -0400

    implemented controller methods

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -1,4 +1,12 @@
 const router = require('express').Router();
+// instead of importing the entire object and having to do pizzaController.getAllPizza(), we destructure the method names out of the imported object and use those names directly
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+} = require('../../controllers/pizza-controller');
 
 // instead of creating duplicate routes for the individual HTTP methods, we can combine them --> router.route('/').get(getCallbackFunction).post(postCallbackFunction); = 
 // router.get('/', getCallbackFunction);
@@ -7,14 +15,15 @@ const router = require('express').Router();
 // set up GET all and POST at /api/pizzas
 router
     .route('/')
-    .get()
-    .post();
+    // see how we simply provide the name of the controller method as the callback; that's why we set up those methods to accept req and res as parameters
+    .get(getAllPizza)
+    .post(createPizza);
 
 // set up GET one, PUT, and DELETE at /api/pizzas/:id
 router
     .route('/:id')
-    .get()
-    .put()
-    .delete();
+    .get(getPizzaById)
+    .put(updatePizza)
+    .delete(deletePizza);
 
 module.exports = router;
